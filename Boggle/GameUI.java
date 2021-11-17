572efd6e7d64129bd7405f3e9d515b7e5c654185
Matthew Peters <matthew.peters.work@gmail.com>
2021-11-17 20:56:42 +0000

    Implemented word validator function in WordEngine

--- Boggle/GameUI.java
+++ Boggle/GameUI.java
@@ -1,16 +1,29 @@
 import javax.swing.*;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
 import java.awt.*;
 
-public class GameUI extends JFrame {
+public class GameUI extends JFrame implements ActionListener{
 	
-	public GameUI(Board b) {
+	private JTextField input;
+	private Board b;
+	private WordEngine w;
+	private JTextField message;
+	
+	
+	public GameUI() {
+		
+		Board b = new Board(8);
+		w = new WordEngine(b);
+		
 		this.setDefaultCloseOperation(EXIT_ON_CLOSE);
 		this.setSize(400,400);
 		this.setLayout(new FlowLayout());
-		
+
 		JPanel grid = new JPanel();
 		grid.setLayout(new GridLayout(b.getSize(),b.getSize()));
 		
+		// add each square in board to a JPanel
 		for (int row = 0; row < b.getSize(); row++) {
 			for (int column = 0; column < b.getSize(); column++) {
 				JTextField square = new JTextField(b.board[row][column].toString());
@@ -19,30 +32,37 @@ public class GameUI extends JFrame {
 			}
 		}
 		
-		
+		//create a JPanel for input
 		JPanel control = new JPanel();
 		control.setLayout(new FlowLayout());
 		JLabel prompt = new JLabel("Enter a word");
-		JTextField input = new JTextField(15);
+		input = new JTextField(15);
 		input.setEditable(true);
+		input.addActionListener(this);
 		control.add(prompt);
 		control.add(input);
 		
-		JTextField message = new JTextField(15);
+		message = new JTextField(15);
 		message.setText("Let's play BOGGLE");
 		message.setEditable(false);
 	
 		this.add(grid);
 		this.add(control);
 		this.add(message);
+		
+		
+		
+	}
+	
+	// when user enters a word
+	public void actionPerformed(ActionEvent e) {
+			String word = input.getText();
+			boolean valid = w.validate(word);
+			message.setText("" + valid);	
 	}
 	
 	public static void main(String[] args) {
-		
-		Board b = new Board(50);
-		WordEngine w = new WordEngine(b);
-		GameUI game = new GameUI(b);
+		GameUI game = new GameUI();
 		game.setVisible(true);
-		
 	}
 }
--- Boggle/WordEngine.java
+++ Boggle/WordEngine.java
@@ -11,8 +11,121 @@ public class WordEngine {
 	
 	
 	public boolean validate(String word) {
-		//TODO - take word as input and determine if it is a valid boggle word for the board
-		return true;
+
+		HashMap<String, int[][]> relevantPairs;
+		relevantPairs = this.getRelevantPairs(word);
+		if (relevantPairs == null) {
+			System.out.println("null");
+			return false;
+		}
+		// get list of first pair in word
+		int[][] firstOccurrence = relevantPairs.get(word.substring(0, 2));
+
+		// create data structure to keep track of used letters
+		String[] usedLetters = new String[2];
+
+		// look for word from each occurrence of first pair until found or exhausted possibilities
+		for (int i = 0; i < firstOccurrence.length; i++) {
+			int r = firstOccurrence[i][2];
+			int c = firstOccurrence[i][3];
+
+			String firstLetter = String.format("%d,%d", firstOccurrence[i][0], firstOccurrence[i][1]);
+			String secondLetter = String.format("%d,%d", firstOccurrence[i][2], firstOccurrence[i][3]);
+
+			// add letters to used letters data structure
+			usedLetters[0] = firstLetter;
+			usedLetters[1] = secondLetter;
+
+			if (join(word, relevantPairs, 0, r, c, usedLetters)) {
+				return true;
+			}
+			// remove letters from used data structure
+			usedLetters[0] = "";
+			usedLetters[1] = "";
+		}
+		return false;
+	}
+
+	private boolean join(String word, HashMap<String, int[][]> relevantPairs, int i, int r, int c,
+			String[] usedLetters) {
+
+		String nextPair = word.substring(i + 1, i + 3);
+		int[][] nextOccurrence = relevantPairs.get(nextPair);
+
+		for (int k = 0; k < nextOccurrence.length; k++) {
+			String letter = String.format("%d,%d", nextOccurrence[k][2], nextOccurrence[k][3]);
+			if (r == nextOccurrence[k][0] && c == nextOccurrence[k][1] && used(usedLetters, letter) == false) {
+				// add to used letters
+				usedLetters = use(usedLetters, letter);
+
+				if (i == word.length() - 3) {
+					for (int y = 0; y < usedLetters.length; y++) {
+						System.out.println(usedLetters[y]);
+					}
+					return true;
+				}
+				if (join(word, relevantPairs, i + 1, nextOccurrence[k][2], nextOccurrence[k][3], usedLetters)) {
+
+					return true;
+				}
+				// remove from used letters
+				usedLetters = unuse(usedLetters, letter);
+			}
+		}
+		return false;
+	}
+	
+	private String[]  use(String[] usedLetters, String letter) {
+		// increase size of array and add new letter to end
+		String[] temp = new String[usedLetters.length+1];
+		for (int i = 0; i < usedLetters.length; i++) {
+			temp[i] = usedLetters[i];
+		}
+		temp[temp.length-1] = letter;
+		usedLetters = temp;
+		return usedLetters;
+	}
+	
+	private String[] unuse(String[] usedLetters, String letter) {
+		// decrease size of array, thereby removing letter from end
+		String[] temp = new String[usedLetters.length-1];
+		for (int i = 0; i < usedLetters.length-1; i++) {
+			temp[i] = usedLetters[i];
+		}
+		usedLetters = temp;
+		return usedLetters;
+	}
+	
+	private boolean used(String[] usedLetters, String letter) {
+		for (int i = 0; i < usedLetters.length; i++) {
+			if (letter.equals(usedLetters[i])) {
+				return true;
+			}
+		}
+		return false;
+	}
+	
+	
+	public HashMap<String, int[][]> getRelevantPairs(String word) {
+		HashMap<String, int[][]> relevantPairs = new HashMap<String, int[][]>();
+		
+		String pair;
+		for (int letterIndex = 0; letterIndex < word.length()-1; letterIndex++) {
+			pair = word.substring(letterIndex, letterIndex+2);
+			String[] occurrences = this.pairs.get(pair);
+			if (occurrences == null) {
+				return null;
+			}
+			int[][] currentPair = new int[occurrences.length][4];
+			for (int i = 0; i < occurrences.length; i++) {
+				String[] stringDigits = occurrences[i].split(",");
+				for (int j = 0; j < 4; j++) {
+					currentPair[i][j] = Integer.parseInt(stringDigits[j]);
+				}	
+			}
+			relevantPairs.put(pair, currentPair);
+		}
+		return relevantPairs;
 	}
 	
 	public HashMap<String, String[]> findPairs(Board b) {
